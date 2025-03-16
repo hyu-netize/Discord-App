25af2dd97f08a09fa82554b03298ec1a713d4ab1
msubu <msubuntu@outlook.com>
2025-03-16 21:48:55 +0900

    config.py

---
+++ config.py
@@ -0,0 +1,30 @@
+class YouTube:
+    API_KEY = "*******************************"
+    DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/*******************************'
+    CHANNEL_IDS = {
+        'チャンネル名': '*******************************',
+    }
+
+class Twitch:
+    CLIENT_ID = '*******************************'
+    CLIENT_SECRET = '*******************************'
+    DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/*******************************'
+    CHANNEL_IDS = {
+        'チャンネル名': '************',
+    }
+
+class TwitCasting:
+    CLIENT_ID = '*******************************'
+    CLIENT_SECRET = '*******************************'
+    DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/*******************************'
+    CHANNEL_IDS = {
+        'チャンネル名': '************',
+    }
+
+class Pokemon:
+    DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/*******************************"
+    MAX_POKEMON_ID = 1025
+
+class Card:
+    DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/*******************************"
+    END_PAGE = 520
